1911a229912a76f827f3fe86c1d3335395b11048
DevUsman <kusman2560@gmail.com>
2022-07-28 16:16:16 +0500

    again filter change

--- pages/grocery/dairy/filter.jsx
+++ pages/grocery/dairy/filter.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Router from 'next/router';
+
 import PageContainer from '~/components/layouts/PageContainer';
 import Newletters from '~/components/partials/commons/Newletters';
 import Product from '~/components/elements/products/DemoProduct';
@@ -7,7 +9,7 @@ import { useEffect } from 'react';
 import Axios from 'axios';
 import { useState } from 'react';
 import Spinner from '~/components/spinner/index';
-import List from "./list";
+import Data from '~/public/static/data/demo';
 
 import { useRouter } from 'next/router';
 const index = () => {
@@ -32,38 +34,68 @@ const index = () => {
         };
         fetchBags();
     }, []);
-
+    const send = (e) => {
+        Router.push({
+            pathname: '/grocery/dairy/filter',
+            query: {
+                e,
+            },
+        });
+    };
     return (
         <CartProvider>
             <PageContainer title="Shop">
                 <div className="ps-page--shop">
                     <div className="ps-container">
-                        
-                       
-                                {/* <WidgetShopCategories /> */}
-                                
-                           
-                            {/* <List/> */}
-                         
-                                <List/>
-                            <div className="d-flex  row">
-                                {loading ? (
-                                    data
-                                        .filter((index) => index.Sub_Cat == e)
-                                        .map((item) => (
-                                            <Product
-                                                key={item}
-                                                image={item?.imgUrl}
-                                                title={item.title}
-                                                price={item.price}
-                                                item={item}
-                                            />
-                                        ))
-                                ) : (
-                                    <Spinner />
-                                )}
+                        {/* <WidgetShopCategories /> */}
+
+                        {/* <List/> */}
+
+                        <div class="card-group mx-auto ">
+                            <div class="dropdown show">
+                                <a
+                                    class="btn btn-secondary dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    id="dropdownMenuLink"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Filters
+                                </a>
+                                <div className="mb-4 ">
+                                    <div
+                                        class="dropdown-menu"
+                                        // key={index}
+                                        aria-labelledby="dropdownMenuLink">
+                                        {Data.Dairy.map((item, index) => (
+                                            <a
+                                                class="dropdown-item"
+                                                onClick={() => send(item.send)}>
+                                                {item.title}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
                             </div>
-                        
+                        </div>
+                        <div className="d-flex  row">
+                            {loading ? (
+                                data
+                                    .filter((index) => index.Sub_Cat == e)
+                                    .map((item) => (
+                                        <Product
+                                            key={item}
+                                            image={item?.imgUrl}
+                                            title={item.title}
+                                            price={item.price}
+                                            item={item}
+                                        />
+                                    ))
+                            ) : (
+                                <Spinner />
+                            )}
+                        </div>
                     </div>
                 </div>
                 <Newletters />
--- pages/grocery/dairy/index.jsx
+++ pages/grocery/dairy/index.jsx
@@ -8,7 +8,8 @@ import Axios from 'axios';
 import { useState } from 'react';
 import Spinner from '~/components/spinner/index';
 import Router from 'next/router';
-import List from "./list"
+import Data from '~/public/static/data/demo';
+
 
 const index = () => {
     const [data, setData] = useState([]);
@@ -42,17 +43,43 @@ const index = () => {
             <PageContainer title="Shop">
                 <div className="ps-page--shop">
                     <div className="ps-container">
-                     
+                    <div class="dropdown show">
+                                <a
+                                    class="btn btn-secondary dropdown-toggle"
+                                    href="#"
+                                    role="button"
+                                    id="dropdownMenuLink"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false">
+                                    Filters
+                                </a>
+                                <div className="mb-4 ">
+                                    <div
+                                        class="dropdown-menu"
+                                        // key={index}
+                                        aria-labelledby="dropdownMenuLink">
+                                        {Data.Dairy.map((item, index) => (
+                                            <a
+                                                class="dropdown-item"
+                                                onClick={() => send(item.send)}>
+                                                {item.title}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
                           
                                 {/* <WidgetShopCategories /> */}
    
                             
                            
-                                <List/>
+                         
                                 <div class="d-flex  row">
                                 {loading ? (
                                     data
-                                        .map((item,index) => (
+                                    .filter((index) => index.Sub_Cat == "LIQUID MILK")
+                                    .map((item) => (
                                             <Product
                                                 key={item}
                                                 image={item?.imgUrl}
@@ -78,83 +105,3 @@ const index = () => {
 
 export default index;
 
-// import React from 'react';
-// import PageContainer from '~/components/layouts/PageContainer';
-// import Newletters from '~/components/partials/commons/Newletters';
-// // import Product from '~/components/elements/products/DemoProduct';
-// import Card from '~/components/elements/products/Card';
-// import { CartProvider } from 'react-use-cart';
-// import { useEffect } from 'react';
-// import Axios from 'axios';
-// import { useState } from 'react';
-// import Router from 'next/router'; 
-// import Data from '~/public/static/data/demo';
-// import List from "./list"
-// import WidgetShopCategories from '~/components/shared/widgets/WidgetShopCategories';
-// const index = () => {
-//     const [data, setData] = useState([]);
-//     const [loading, setLoading] = useState(false);
-//     useEffect(() => {
-//         const fetchBags = async () => {
-//             try {
-//                 const data = await Axios.get(
-//                     `https://dawoodddocker.herokuapp.com/api/v1/product/id/84`
-//                 );
-//                 setData(data.data.data);
-//                 setLoading(true);
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         };
-//         fetchBags();
-//     }, []);
-
-//     const send = (e) => {
-//         Router.push({
-//             pathname: '/grocery/dairy/filter',
-//             query: {
-//                 e,
-//             },
-//         });
-//     };
-
-//     return (
-//         <CartProvider>
-//             <PageContainer title="Shop">
-//                 <div className="ps-page--shop ">
-//                     <div className="ps-container">
-//                         <div>
-
-//                         </div>
-//                         <div className="ps-layout--shop">
-//                             <div className="row mt-4">
-//                                 <div class="card-group mx-auto">
-//                                 {/* <WidgetShopCategories /> */}
-//                                     <List/>
-//                                     {Data.Dairy.map((item, index) => (
-//                                         <div className="mb-4  ">
-                                            
-//                                             {/* <ul classname="list-group">
-//                                                 <a onClick={() => send(item.send)} ><li classname="list-group-item">{item.title}</li></a>
-                                            
-//                                             </ul> */}
-//                                             {/* <Card
-//                                                 key={index}
-//                                                 img={item.url}
-//                                                 title={item.title}
-//                                                 onclick={() => send(item.send)}
-//                                             /> */}
-//                                         </div>
-//                                     ))}
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <Newletters />
-//             </PageContainer>
-//         </CartProvider>
-//     );
-// };
-
-// export default index;
