f7b3e0ff5acd3a6e79b1947fa85dc9ea30224161
wabbi-khan <kwahab789@gmail.com>
2022-09-13 16:29:53 +0500

    search setting

--- components/shared/headers/modules/SearchHeader.jsx
+++ components/shared/headers/modules/SearchHeader.jsx
@@ -22,10 +22,12 @@ const SearchHeader = () => {
                 setLoading(true);
                 const response = await axios.get(
                     // `http://localhost:8080/api/v1/product?query=${keyword}`
-                    `https://dawoodddocker.herokuapp.com/api/v1/product?query=${keyword}`
+                    // `https://dawoodddocker.herokuapp.com/api/v1/product?query=${keyword}`
+                    `http://localhost:5000/users?query=${keyword}`
                 );
 
-                setResultItems(response.data.data);
+                // setResultItems(response.data.data);
+                setResultItems(response.data);
                 setIsSearch(true);
                 setLoading(false);
             };
