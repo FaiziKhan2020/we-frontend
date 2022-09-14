6a1f20bcd53f989fc9ed46f6edcc789c97e7ffa6
wabbi-khan <kwahab789@gmail.com>
2022-09-14 14:56:10 +0500

    k

--- components/QAhomePage/ProductCard.jsx
+++ components/QAhomePage/ProductCard.jsx
@@ -4,7 +4,7 @@ import { useCart } from 'react-use-cart';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 const ProductCard = ({ imgUrl, title, price, item }) => {
-    const { addItem } = useCart();
+    // const { addItem } = useCart();
     const [isAdding, setIsAdding] = useState(false);
     const total = () => {
         setIsAdding(true);
@@ -31,17 +31,21 @@ const ProductCard = ({ imgUrl, title, price, item }) => {
                     <div className="font-weight-bold">{title}</div>{' '}
                     <div>QR. {price} </div>
                 </div>
-                <motion.button
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    type="button"
-                    className={`${isAdding ? 'btn btn-primary' : 'btn'}`}
+                <Link
+                    href="/product/[pid]"
+                    as={`/product/${item?.id}`}
                     onClick={() => {
-                        addItem(item);
                         total();
                     }}>
-                    ADD{isAdding ? 'ED' : ''}
-                </motion.button>
+                    <motion.button
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        type="button"
+                        onClick={() => total()}
+                        className={`${isAdding ? 'btn btn-primary' : 'btn'}`}>
+                        Buy {isAdding ? 'Now' : ''}
+                    </motion.button>
+                </Link>
             </div>
         </div>
     );
--- components/elements/detail/modules/ModuleDetailActionsMobile.jsx
+++ components/elements/detail/modules/ModuleDetailActionsMobile.jsx
@@ -3,10 +3,28 @@ import { connect, useDispatch } from 'react-redux';
 import { useCart } from 'react-use-cart';
 import { useRouter } from 'next/router';
 import Select from 'react-select';
+import { motion } from 'framer-motion';
+import style from '~/components/QAhomePage/style.module.css';
 const ModuleDetailActionsMobile = ({ product }) => {
-    const [Gender, setGender] = useState('');
     const { addItem } = useCart();
+    const [isAdding, setIsAdding] = useState(false);
+    const total = () => {
+        setIsAdding(true);
+        setTimeout(() => {
+            setIsAdding(false);
+        }, 1000);
+    };
+    const [Gender, setGender] = useState('');
     const Router = useRouter();
+    function AddToCart() {
+        addItem({
+            id: product.id,
+            price: product.price,
+            title: product.title,
+            imgUrl: product.imgUrl,
+            sizes: Gender,
+        });
+    }
     function handleBuynow(e) {
         e.preventDefault();
         addItem({
@@ -30,7 +48,21 @@ const ModuleDetailActionsMobile = ({ product }) => {
                     isClearable
                     placeholder="Size"
                     onChange={(e) => setGender(e.value)}
+                    re
                 />
+            </div>
+            <div className={style.moduleBtn}>
+                <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    type="button"
+                    className={`${isAdding ? 'btn btn-primary' : 'btn'}`}
+                    onClick={() => {
+                        AddToCart();
+                        total();
+                    }}>
+                    ADD{isAdding ? 'ED' : ''}
+                </motion.button>
             </div>
             <a
                 className="ps-btn my-4"
