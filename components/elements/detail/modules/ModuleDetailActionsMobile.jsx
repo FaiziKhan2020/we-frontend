6a68b4c816034cb02aecd7af3c26efd3e1ee30ad
wabbi-khan <kwahab789@gmail.com>
2022-09-24 12:40:44 +0500

    ok

--- components/elements/detail/modules/ModuleDetailActionsMobile.jsx
+++ components/elements/detail/modules/ModuleDetailActionsMobile.jsx
@@ -32,55 +32,58 @@ const ModuleDetailActionsMobile = ({ product }) => {
     console.log(items);
     return (
         <Form onFinish={AddToCart}>
-            <Form.Item
-                name="firstName"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Enter your first name!',
-                    },
-                ]}>
-                <select
-                    className={`mr-4 mb-4 ${style.selectpro}`}
-                    onChange={(e) => setColor(e.target.value)}
-                    required
-                    value={Color}>
-                    <option>Choose Color</option>
-                    {product.color[0].map((item) => (
-                        <>
-                            <option> {item} </option>
-                        </>
-                    ))}
-                </select>
-            </Form.Item>
-            <Form.Item
-                name="firstName"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Enter your first name!',
-                    },
-                ]}>
-                <select
-                    className={style.selectpro}
-                    onChange={(e) => setSize(e.target.value)}
-                    required
-                    value={Size}>
-                    <option> Choose Size</option>
-                    {product.sizes[0].map((item) => (
-                        <>
-                            <option> {item} </option>
-                        </>
-                    ))}
-                </select>
-            </Form.Item>
+            <div className={style.MainSelect}>
+                <Form.Item
+                    name="Color"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Choose Color',
+                        },
+                    ]}>
+                    <select
+                        className={`mr-4 ${style.selectpro}`}
+                        onChange={(e) => setColor(e.target.value)}
+                        required
+                        value={Color}>
+                        <option>Choose Color</option>
+                        {product.color[0].map((item) => (
+                            <>
+                                <option> {item} </option>
+                            </>
+                        ))}
+                    </select>
+                </Form.Item>
+                <Form.Item
+                    name="Size"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Choose Size',
+                        },
+                    ]}>
+                    <select
+                        className={style.selectpro}
+                        onChange={(e) => setSize(e.target.value)}
+                        required
+                        value={Size}>
+                        <option> Choose Size</option>
+                        {product.sizes[0].map((item) => (
+                            <>
+                                <option> {item} </option>
+                            </>
+                        ))}
+                    </select>
+                </Form.Item>
+            </div>
+
             <div className={style.moduleBtn}>
                 <motion.button
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                     type="submit"
-                    className="btn btn-primary">
-                    ADD
+                    className={`mt-4 ${isAdding ? 'btn btn-primary' : 'btn'}`}>
+                    ADD{isAdding ? 'ED' : ''}
                 </motion.button>
             </div>
         </Form>
