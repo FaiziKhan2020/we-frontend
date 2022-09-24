80d6419925c264e706b27a7c93caf05e21669b98
wabbi-khan <kwahab789@gmail.com>
2022-09-24 12:42:36 +0500

    minors changes

--- pages/page/about.jsx
+++ pages/page/about.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BreadCrumb from '~/components/elements/BreadCrumb';
 import FooterDefault from '~/components/shared/footers/FooterDefault';
 import PageContainer from '~/components/layouts/PageContainer';
-
+import { CartProvider } from 'react-use-cart';
 const AboutUs = () => {
     const breadCrumb = [
         {
@@ -15,57 +15,60 @@ const AboutUs = () => {
     ];
 
     return (
-        <PageContainer footer={<FooterDefault />} title="Blank page">
-            <div className="ps-page--single">
-                <BreadCrumb breacrumb={breadCrumb} layout="fullwidth" />
-                <div className="ps-section--custom">
-                    <div className="container">
-                        <div className="ps-section__header">
-                            <h3>About Us</h3>
-                        </div>
-                        <div className="ps-section__content">
-                            <p>
-                                The best online shoping store in Qatar is
-                                qfashiontrends.com. Anywhere in Doha can receive
-                                our deliveries for free.
-                            </p>
-                            <p>
-                                We at qfashiontrends are committed to making it
-                                possible for customers to find a variety of
-                                products at affordable costs. Our platform
-                                provides user-friendly services that guarantee
-                                quick answers to any questions that our
-                                consumers could have.
-                            </p>
-                            <p>
+        <CartProvider>
+            <PageContainer footer={<FooterDefault />} title="Blank page">
+                <div className="ps-page--single">
+                    <BreadCrumb breacrumb={breadCrumb} layout="fullwidth" />
+                    <div className="ps-section--custom">
+                        <div className="container">
+                            <div className="ps-section__header">
+                                <h3>About Us</h3>
+                            </div>
+                            <div className="ps-section__content">
+                                <p>
+                                    The best online shoping store in Qatar is
+                                    qfashiontrends.com. Anywhere in Doha can
+                                    receive our deliveries for free.
+                                </p>
+                                <p>
+                                    We at qfashiontrends are committed to making
+                                    it possible for customers to find a variety
+                                    of products at affordable costs. Our
+                                    platform provides user-friendly services
+                                    that guarantee quick answers to any
+                                    questions that our consumers could have.
+                                </p>
+                                <p>
+                                    <b
+                                        className="pr-4"
+                                        style={{
+                                            fontSize: '18px',
+                                            color: 'black',
+                                        }}>
+                                        Vision:
+                                    </b>
+                                    To be the QATAR's top customer-centric
+                                    business where clients can find whatever
+                                    they would want to buy online and choose
+                                    from a huge variety of products at
+                                    affordable costs.
+                                </p>
                                 <b
                                     className="pr-4"
                                     style={{
                                         fontSize: '18px',
                                         color: 'black',
                                     }}>
-                                    Vision:
+                                    Mission:
                                 </b>
-                                To be the QATAR's top customer-centric business
-                                where clients can find whatever they would want
-                                to buy online and choose from a huge variety of
-                                products at affordable costs.
-                            </p>
-                            <b
-                                className="pr-4"
-                                style={{
-                                    fontSize: '18px',
-                                    color: 'black',
-                                }}>
-                                Mission:
-                            </b>
-                            To offer customers the greatest online shopping
-                            portal possible.
+                                To offer customers the greatest online shopping
+                                portal possible.
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </PageContainer>
+            </PageContainer>
+        </CartProvider>
     );
 };
 
